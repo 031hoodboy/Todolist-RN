94598b1dc85741289efaa026ce1ee5383eedb38d
hoodboy <littlepark0107@gmail.com>
2021-06-15 20:21:18 +0900

    Feat: complete

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import React, {useState} from 'react';
+import TodoList from './components/TodoList';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+import TodoInsert from './components/TodoInsert';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useFonts } from 'expo-font';
 
-export default function App() {
+Icon.loadFont();
+
+
+const App = () => {
+  // todos: {id: Number, textValue: string, checked: boolean }
+  const [loaded] = useFonts({
+    Montserrat: require('./assets/fonts/Montserrat-SemiBold.ttf'),
+  });
+  
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = text => {
+    setTodos([
+      ...todos,
+      {id: Math.random().toString(), textValue: text, checked: false},
+    ]);
+  };
+
+  const onRemove = id => e => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const onToggle = id => e => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? {...todo, checked: !todo.checked} : todo,
+      ),
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.appTitle}>Hello Todolist</Text>
+      <View style={styles.card}>
+        <TodoInsert onAddTodo={addTodo} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  appTitle: {
+    color: '#444',
+    fontSize: 36,
+    marginTop: 40,
+    marginBottom: 30,
+    padding: 10,
+    fontWeight: '800',
+    textAlign: 'center',
+    // backgroundColor: '#f29d4c',
+    fontFamily: 'Pangolin',
+  },
+  card: {
+    backgroundColor: '#fff',
+    flex: 1,
+    borderTopLeftRadius: 10, // to provide rounded corners
+    borderTopRightRadius: 10, // to provide rounded corners
+    marginLeft: 10,
+    marginRight: 10,
   },
 });
+
+export default App;
